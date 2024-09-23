e41c30d4119a7ad26869edbe455074113391dab3
ramyadosapati <ramya.b22@gmail.com>
2024-09-23 09:46:34 +0530

    adding myntra.java

---
+++ Myntra.java
@@ -0,0 +1,14 @@
+package demo.sessions;
+
+public class Myntra {
+	Myntra()
+	{
+		System.out.println("constuctor");
+	}
+
+	public static void main(String[] args) {
+		Myntra a1=new Myntra(); 
+
+	}
+
+}
