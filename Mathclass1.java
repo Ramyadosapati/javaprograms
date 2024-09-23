90a96b9c972330330e544422d0cec6f44c0dcf25
ramyadosapati <ramya.b22@gmail.com>
2024-09-23 16:44:06 +0530

    mathclass1.java

---
+++ Mathclass1.java
@@ -0,0 +1,14 @@
+package scan;
+
+public class Mathclass1
+{
+
+	public static void main(String[] args) throws InterruptedException {
+		for(int i=1;i<5;i++)
+		{
+		System.out.println(Math.random());
+		Thread.sleep(1000);
+		}
+	}
+
+}
