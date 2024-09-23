2864f1c69755699e2d510cd98faae89f456e7218
ramyadosapati <ramya.b22@gmail.com>
2024-09-23 16:32:30 +0530

    areaofcircle.java

---
+++ Areaofcircle.java
@@ -0,0 +1,14 @@
+package scan;
+
+public class Areaofcircle
+{
+	static final   double  pivalue =Math.PI;
+
+	public static void main(String[] args) {
+		double radius =Math.random();
+		double area =pivalue*radius*radius;
+		System.out.println(area);
+
+	}
+
+}
