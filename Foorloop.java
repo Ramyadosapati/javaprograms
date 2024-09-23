11434941cbc4bacb476494b3cb08a38c9420f5f0
ramyadosapati <ramya.b22@gmail.com>
2024-09-23 15:30:01 +0530

    forloop.java

---
+++ Foorloop.java
@@ -0,0 +1,13 @@
+package demo.sessions;
+
+public class Foorloop {
+
+	public static void main(String[] args) {
+		for(int i=1;i<5;i++) {
+			System.out.println("ramya");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
