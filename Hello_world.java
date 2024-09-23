c0b595b4a910ca2efaaa562fa17ddc849feb80e3
ramyadosapati <ramya.b22@gmail.com>
2024-09-23 09:42:28 +0530

    adding hellow world

---
+++ Hello_world.java
@@ -0,0 +1,30 @@
+package demo.sessions;
+
+public class Hello_world {
+
+static void method1()
+{
+	System .out.println("ramya");
+}
+
+static void  method2()
+{
+	System .out.println("praven");
+}
+static void  method3()
+{
+	System.out.println("charvi");
+}
+static void   method4 ()
+{
+	System.out .println("chinnu");
+}
+	public static void main (String [] args) {
+		System.out.println("HELLO WORLD");
+	method1 ();
+	method2 ();
+	method3();
+	method4();
+	}
+
+}
